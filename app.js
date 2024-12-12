ca2c58159c919d1f4a5679eb7ebe97454515cf25
Saranraj <saran@atatus.com>
2024-12-12 15:31:18 +0530

    intial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send("oky")
+})
+
+app.listen('3000', (req, res) => {
+  console.log(`app listen http://127.0.0.1:3000`)
+})
