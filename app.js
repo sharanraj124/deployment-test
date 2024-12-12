cb80f7c78e1dc0783703835f3e224fb37979401a
Saranraj <saran@atatus.com>
2024-12-12 18:43:42 +0530

    test new version

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
   res.send("oky")
 })
 
+app.get('/send', (req, res) => {
+  res.send("oky")
+})
+
 app.listen('3000', (req, res) => {
   console.log(`app listen http://127.0.0.1:3000`)
 })
